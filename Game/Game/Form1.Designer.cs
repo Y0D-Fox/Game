fd91f81d5db980a839c1a448ec251b1688f6a55e
Y0D-Fox <132070455+Y0D-Fox@users.noreply.github.com>
2023-06-22 09:37:33 +0300

    Add files via upload

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Game
+{
+    partial class Spider_Game
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tag = "Time";
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::Game.Properties.Resources.Spider;
+            this.pictureBox1.Location = new System.Drawing.Point(193, 197);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Очки:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Нажмите старт";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(53, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " 0";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tag = "Move";
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tag = "Live_Spider1";
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tag = "New-Spider";
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1000;
+            this.timer5.Tag = "Deid";
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = global::Game.Properties.Resources.Spider;
+            this.pictureBox2.Location = new System.Drawing.Point(483, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Image = global::Game.Properties.Resources.Spider;
+            this.pictureBox3.Location = new System.Drawing.Point(296, 83);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 98);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseMove);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 1000;
+            this.timer6.Tag = "Live_Spider2";
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Interval = 1000;
+            this.timer7.Tag = "Live_Spider3";
+            this.timer7.Tick += new System.EventHandler(this.timer7_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Время жизни:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(96, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // Spider_Game
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 436);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Spider_Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Spider_Game";
+            this.Load += new System.EventHandler(this.Spider_Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Spider_Game : Form
+
+    {
+        int timerlive=0;
+        Bitmap bmp = new Bitmap(816, 475);
+        Random rnd = new Random();
+        int i = 4;
+        int click;
+        public Spider_Game()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+    //не рабочая кнопка
+            timer1.Enabled = true;
+
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        label3.Text = "0";
+
+            Properties.Settings.Default.point=0;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            timer1.Enabled = true;
+
+            //timer5.Enabled = true;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            click++; 
+            Properties.Settings.Default.point++;
+            label3.Text = Convert.ToString(Properties.Settings.Default.point);
+            pictureBox1 .Visible = false;
+            timer3.Enabled = true;
+            timer5.Enabled = true;
+        }
+
+        public void Spider_Game_Load(object sender, EventArgs e)
+        {
+            timer5.Interval = 1000;
+            label3.Text = Convert.ToString(Properties.Settings.Default.point);
+            for (int y = 0; y < 475; y++)
+            {
+                for (int x = 0; x < 816; x++)
+                {
+                 
+
+                }
+            }
+            timer4.Enabled = true;
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            i--;
+            label2.Text = i.ToString();
+
+            if (i == 0)
+            {
+                label2.Text = "Дави их";
+                pictureBox1.Visible = true;
+                timer5.Enabled = true;
+                timer1.Stop();
+            }
+            else if (i < 0)
+            {
+                i = 4;
+            }
+            timer2.Enabled = true;
+           
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+          
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point cursor = me.Location;
+            if (cursor.X - 1 >= 0 && cursor.X + 1 <= 500 && cursor.Y - 1 >= 0 && cursor.Y + 1 <= 400);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+         
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int x = rnd.Next(20, 500);
+            int y = rnd.Next(50, 400);
+            int x1 = rnd.Next(30, 495);
+            int y1 = rnd.Next(25, 365);
+            int x2 = rnd.Next(41, 450);
+            int y2 = rnd.Next(27, 395);
+            pictureBox1.Top = y;
+            pictureBox1.Left = x; 
+            pictureBox2.Top = y1;
+            pictureBox2.Left = x1;
+            pictureBox3.Top = y2;
+            pictureBox3.Left = x2;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Visible==false){ 
+            pictureBox1.Visible = false;
+            Task.Delay(10000);
+            pictureBox1.Visible = true; }
+          
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.point == 15)
+            {
+                pictureBox2.Visible = true;
+                timer6.Enabled = true;
+            }
+            else if (Properties.Settings.Default.point == 30)
+            {
+                pictureBox3.Visible = true;
+                timer7.Enabled = true;
+            }
+            else if (Properties.Settings.Default.point == 60)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = false;
+                pictureBox3.Visible = false;
+                timer5.Enabled = false;
+                timer3.Enabled = false;
+                timer6.Enabled = false;
+                timer7.Enabled = false;
+                DialogResult dialogResult = MessageBox.Show("Поздравляю вы выйграли", "Ура!!!", MessageBoxButtons.OK);
+                if (dialogResult == DialogResult.OK)
+                {
+                    Close();
+                }
+            }
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            timer5.Enabled = true; 
+            click++; 
+            Properties.Settings.Default.point++;
+            label3.Text = Convert.ToString(Properties.Settings.Default.point);
+            pictureBox2.Visible = false;
+
+            timer6.Enabled = true;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            timer5.Enabled = true;
+            click++; 
+            Properties.Settings.Default.point++;
+            label3.Text = Convert.ToString(Properties.Settings.Default.point);
+            pictureBox3.Visible = false;
+
+            timer7.Enabled = true;
+        }
+
+        private void pictureBox3_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point cursor = me.Location;
+            if (cursor.X - 1 >= 0 && cursor.X + 1 <= 500 && cursor.Y - 1 >= 0 && cursor.Y + 1 <= 400) ;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point cursor = me.Location;
+            if (cursor.X - 1 >= 0 && cursor.X + 1 <= 500 && cursor.Y - 1 >= 0 && cursor.Y + 1 <= 400);
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox2.Visible == false)
+            {
+                pictureBox2.Visible = false;
+                Task.Delay(10000);
+                pictureBox2.Visible = true;
+            }
+        }
+
+        private void timer7_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox3.Visible == false)
+            {
+                pictureBox3.Visible = false;
+                Task.Delay(10000);
+                pictureBox3.Visible = true;
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            timerlive++;
+            label5.Text = timerlive.ToString();
+            timer5.Start();
+            if (timerlive == 10 && click >= 3)
+            {
+                    click = 0;
+                    timerlive = 0;
+                    label5.Text = timerlive.ToString("0");
+            }
+           if (click <= 3 && timerlive == 10)
+           { timer1.Stop();
+              timer2.Stop();
+                timer5.Stop(); 
+                DialogResult dialogResult = MessageBox.Show("Игра окончена", "Вы проиграли >:|", MessageBoxButtons.OK);
+                if (dialogResult == DialogResult.OK)
+                {
+                    Close();
+                }
+               
+            }
+        }
+
+        private void timer8_Tick(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
